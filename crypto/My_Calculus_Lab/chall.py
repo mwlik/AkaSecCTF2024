66ae578fbe5fa418ddd0fb3ab9258a0954439d5e
Malik Mouhiidine <malikprofessional2020@gmail.com>
2024-02-23 18:14:27 +0100

    soemthing

--- crypto/My_Calculus_Lab/chall.py
+++ crypto/My_Calculus_Lab/chall.py
@@ -20,14 +20,7 @@ def encrypt(message, key):
     point = f.subs(x, key)
     return [(ord(char) + int(point.evalf())) for char in message]
 
-def decrypt(message, key):
-    global f
-    global x
-    point = f.subs(x, key)
-    return [(chr(int(code - int(point.evalf())))) for code in message]
-
 encrypted = encrypt(FLAG, key)
-decrypted = decrypt(encrypted, key)
 
+print(f"Key: {key}")
 print(f"Encrypted: {encrypted}")
-print(f"Decrypted: {decrypted}")
