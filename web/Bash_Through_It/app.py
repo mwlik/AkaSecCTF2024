526b3673c8e01756d6edb6dcf05ea349c6c3cced
Malik Mouhiidine <malikprofessional2020@gmail.com>
2024-02-26 18:15:41 +0100

    soemthing

--- web/Bash_Through_It/app.py
+++ web/Bash_Through_It/app.py
@@ -8,4 +8,14 @@ def calculate():
     comment = request.form.get('comment')
     if not comment:
         return 'No comment provided', 400
-    return subprocess.check_output('echo "A really simple command that needs a comment to explain it" # ' + comment, shell=True)
+    comment = comment.replace('flag.txt', '')
+    comment = comment.replace('*', '')
+    try:
+        output = subprocess.check_output('echo "A really simple echo that needs a comment to explain it" # ' + comment, shell=True)
+    except subprocess.CalledProcessError as e:
+        return 'Error', 500
+    return 'Comment added successfully!', 200
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
