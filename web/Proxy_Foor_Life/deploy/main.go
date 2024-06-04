12836b52cc36c13fe7799a5df63b71666dcf8ee6
Malik Mouhiidine <malikprofessional2020@gmail.com>
2024-06-04 21:14:16 +0100

    Update

---
+++ web/Proxy_Foor_Life/deploy/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+    "io"
+    "fmt"
+    "net/http"
+    "html/template"
+    "github.com/doyensec/safeurl"
+    _ "net/http/pprof"
+)
+
+var templates = template.Must(template.ParseGlob("templates/*.html"))
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method == http.MethodPost {
+        config := safeurl.GetConfigBuilder().
+            Build()
+
+        client := safeurl.Client(config)
+
+        url := r.FormValue("url")
+
+        _, err := client.Get(url)
+        if err != nil {
+            renderTemplate(w, "index", map[string]string{"error": "The URL you entered is dangerous and not allowed."})
+            fmt.Println(err)
+            return
+        }
+
+        resp, err := http.Get(url)
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        defer resp.Body.Close()
+
+        body, _ := io.ReadAll(resp.Body)
+        renderTemplate(w, "index", map[string]interface{}{"result": template.HTML(body)})
+        return
+    }
+
+    renderTemplate(w, "index", nil)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+    err := templates.ExecuteTemplate(w, tmpl+".html", data)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+func main() {
+    http.HandleFunc("/", indexHandler)
+    http.ListenAndServe(":1337", nil)
+}
